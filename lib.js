95cebeb4e186996f66a30bd218b9cd74578c5d7c
alxgmv <alxgmv@gmail.com>
2020-01-02 11:46:24 +0100

    update_1
    working version

---
+++ lib.js
@@ -0,0 +1,57 @@
+let x = 0;
+function randomNumber() {
+	x = Math.floor((Math.random() * 1000) + 1);
+	return x;
+}
+
+console.log(randomNumber());
+
+function counter() {
+	let currentAttempt = 1;
+		return function() {
+			return currentAttempt++;
+		}
+}
+
+let attempt = counter();
+
+window.guessNumber = function() {
+	// console.log(attempt()-1);
+	if (parseFloat(attempt()) <= 10) {
+		const num = prompt('Enter your number');
+		if (!isNaN(parseFloat(num)) && isFinite(num)) {
+			if (+num === x) {
+				alert('Your number is the same');
+				const res = confirm('One more time?');
+				if (res) {
+					randomNumber();
+					console.log(randomNumber());
+					guessNumber();
+				} else {
+					alert('Canceled');
+				}
+			} else if (+num > x) {
+				alert('Your number is too big');
+				guessNumber();
+			} else {
+				alert('Your number is too small');
+				guessNumber();
+			}
+		} else if (num === null) {
+			alert('Canceled');
+		} else {
+			alert('Enter valid number!');
+			guessNumber();
+		}
+	} else {
+		let y = confirm('Maximumn attempts exceed, one more time?');
+		if (y) {
+			attempt = counter();
+			randomNumber();
+			guessNumber();
+		} else {
+			alert('Cancelled');
+		}
+	}
+}
+// guessNumber();
